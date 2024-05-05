79917cffeeaebc003c0dff94f4e62af61b3e76b0
Veolinan <104050582+Veolinan@users.noreply.github.com>
2024-05-05 04:30:38 +0300

    improve

--- content_scripts/autofill/FormAutofill.js
+++ content_scripts/autofill/FormAutofill.js
@@ -6,75 +6,64 @@
 
 const EXPORTED_SYMBOLS = ["FormAutofill"];
 
-const LOG_LEVEL = "extensions.formautofill.loglevel";
+const LOG_LEVEL_PREF = "extensions.formautofill.loglevel";
 const DEBUG = "debug";
 
 const ADDRESSES_FIRST_TIME_USE_PREF = "extensions.formautofill.firstTimeUse";
-const AUTOFILL_CREDITCARDS_AVAILABLE_PREF =
-  "extensions.formautofill.creditCards.available";
+const CREDITCARDS_AVAILABLE_PREF = "extensions.formautofill.creditCards.available";
 const CREDITCARDS_USED_STATUS_PREF = "extensions.formautofill.creditCards.used";
-const ENABLED_AUTOFILL_ADDRESSES_PREF =
-  "extensions.formautofill.addresses.enabled";
-const ENABLED_AUTOFILL_ADDRESSES_CAPTURE_PREF =
-  "extensions.formautofill.addresses.capture.enabled";
-const ENABLED_AUTOFILL_CREDITCARDS_PREF =
-  "extensions.formautofill.creditCards.enabled";
-const ENABLED_AUTOFILL_CREDITCARDS_REAUTH_PREF =
-  "extensions.formautofill.reauth.enabled";
-const AUTOFILL_CREDITCARDS_HIDE_UI_PREF =
-  "extensions.formautofill.creditCards.hideui";
+const ADDRESSES_ENABLED_PREF = "extensions.formautofill.addresses.enabled";
+const ADDRESSES_CAPTURE_ENABLED_PREF = "extensions.formautofill.addresses.capture.enabled";
+const CREDITCARDS_ENABLED_PREF = "extensions.formautofill.creditCards.enabled";
+const CREDITCARDS_REAUTH_ENABLED_PREF = "extensions.formautofill.reauth.enabled";
+const CREDITCARDS_HIDE_UI_PREF = "extensions.formautofill.creditCards.hideui";
 const SUPPORTED_COUNTRIES_PREF = "extensions.formautofill.supportedCountries";
 
 class FormAutofill {
   constructor() {
     this.DEFAULT_REGION = Region.home || "US";
-    this.debug = this.log.debug.bind(this.log);
+    this.debug = this.log.bind(this.log, "FormAutofill: ");
     this.initializeLogging();
   }
 
   initializeLogging() {
-    const { ConsoleAPI } = ChromeUtils.import(
-      "resource://gre/modules/Console.jsm"
-    );
+    const { ConsoleAPI } = ChromeUtils.import("resource://gre/modules/Console.jsm");
     this.log = new ConsoleAPI({
-      maxLogLevelPref: LOG_LEVEL,
+      maxLogLevelPref: LOG_LEVEL_PREF,
       prefix: "FormAutofill",
     });
-    if (Services.prefs.getCharPref(LOG_LEVEL).toLowerCase() === DEBUG) {
+
+    if (Services.prefs.getCharPref(LOG_LEVEL_PREF).toLowerCase() === DEBUG) {
       this.debug("Debug logging enabled");
     }
   }
 
   get isAutofillEnabled() {
-    return (
-      this.isAutofillAddressesEnabled || this.isAutofillCreditCardsEnabled
-    );
+    return this.isAutofillAddressesEnabled || this.isAutofillCreditCardsEnabled;
   }
 
   get isAutofillCreditCardsEnabled() {
-    return (
-      this.isAutofillCreditCardsAvailable && this._isAutofillCreditCardsEnabled
-    );
+    return this.isAutofillCreditCardsAvailable && this._isAutofillCreditCardsEnabled;
   }
 
   get isAutofillAddressesEnabled() {
-    return Services.prefs.getBoolPref(ENABLED_AUTOFILL_ADDRESSES_PREF);
+    return Services.prefs.getBoolPref(ADDRESSES_ENABLED_PREF);
   }
 
   get isAutofillAddressesCaptureEnabled() {
-    return Services.prefs.getBoolPref(ENABLED_AUTOFILL_ADDRESSES_CAPTURE_PREF);
+    return Services.prefs.getBoolPref(ADDRESSES_CAPTURE_ENABLED_PREF);
   }
 
   get isAutofillCreditCardsAvailable() {
-    return Services.prefs.getBoolPref(AUTOFILL_CREDITCARDS_AVAILABLE_PREF);
+    return Services.prefs.getBoolPref(CREDITCARDS_AVAILABLE_PREF);
   }
 
   get _isAutofillCreditCardsEnabled() {
-    return Services.prefs.getBoolPref(ENABLED_AUTOFILL_CREDITCARDS_PREF);
+    return Services.prefs.getBoolPref(CREDITCARDS_ENABLED_PREF);
   }
 
   get isAutofillCreditCardsHideUI() {
-    return Services.prefs.getBoolPref(AUTOFILL_CREDITCARDS_HIDE_UI_PREF);
+    return Services.prefs.getBoolPref(CREDITCARDS_HIDE_UI_PREF);
   }
 
   get isAutofillAddressesFirstTimeUse() {
@@ -87,7 +76,7 @@ class FormAutofill {
 
   get supportedCountries() {
     const countryCodes = Services.prefs.getCharPref(SUPPORTED_COUNTRIES_PREF);
-    return countryCodes.split(",");
+    return countryCodes.split(",").map(countryCode => countryCode.trim());
   }
 }
 
