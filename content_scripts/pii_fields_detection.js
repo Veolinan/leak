2a92f5c0fcaa97b9b3bf9bb209254d06826d9a12
Veolinan <104050582+Veolinan@users.noreply.github.com>
2024-05-05 04:30:17 +0300

    improve

--- content_scripts/pii_fields_detection.js
+++ content_scripts/pii_fields_detection.js
@@ -1,32 +1,23 @@
 function getAutofillElements() {
-  let autoFillElements = [];
-  try {
-    const inputEls = Array.from(document.getElementsByTagName("input"));
-    const autofillResultsByForms = FormAutofillHeuristics.getFormInfo(
-      inputEls,
-      false
-    );
-    for (const form of autofillResultsByForms) {
-      let elements = form.fieldDetails.map((formEl) => ({ ...formEl }));
-      let shownElements = elements.map((el) => ({
-        fieldName: el.fieldName,
-        element: el.element,
-        isShown: isShown(el.element),
-      }));
-      autoFillElements = autoFillElements.concat(shownElements);
-    }
-  } catch (error) {
-    // console.log(`Error occured while finding autofill elements: `, error.message);
-  }
-  return autoFillElements;
+  const inputEls = Array.from(document.getElementsByTagName("input"));
+  const autofillResultsByForms = FormAutofillHeuristics.getFormInfo(inputEls, false);
+  return autofillResultsByForms.flatMap((form) => {
+    const elements = form.fieldDetails.map((formEl) => ({ ...formEl }));
+    const shownElements = elements.map((el) => ({
+      fieldName: el.fieldName,
+      element: el.element,
+      isShown: isShown(el.element),
+    }));
+    return shownElements;
+  });
 }
 
 function _hasLabelMatchingRegex(element, regex) {
-  // @ts-ignore
-  if (element.labels !== null && element.labels.length) {
-    // @ts-ignore
-    if (regex.test(element.labels[0].textContent)) {
-      return true;
+  if (element.labels && element.labels.length) {
+    for (const label of element.labels) {
+      if (regex.test(label.textContent)) {
+        return true;
+      }
     }
   }
 
@@ -35,10 +26,9 @@ function _hasLabelMatchingRegex(element, regex) {
 
 function isInferredUsernameField(element) {
   const expr =
-    /user.name|username|display.name|displayname|user.id|userid|screen.name|screenname|benutzername|benutzer.name|nickname|profile.name|profilename/i;
+    /user\.name|username|display\.name|displayname|user\.id|userid|screen\.name|screenname|benutzername|benutzer\.name|nickname|profile\.name|profilename/i;
 
-  let ac = element.autocomplete ? element.autocomplete.fieldName : undefined;
-  if (ac && ac === "username") {
+  if (element.autocomplete && element.autocomplete.fieldName === "username") {
     return true;
   }
 
@@ -55,45 +45,33 @@ function isInferredUsernameField(element) {
 function _elementAttrsMatchRegex(element, regex) {
   if (
     regex.test(element.id) ||
-    // @ts-ignore
     regex.test(element.name) ||
     regex.test(element.className)
   ) {
     return true;
   }
 
-  let placeholder = element.getAttribute("placeholder");
+  const placeholder = element.getAttribute("placeholder");
   if (placeholder && regex.test(placeholder)) {
     return true;
   }
+
   return false;
 }
 
 function getUsernameFields() {
-  let usernameFields = [];
-  try {
-    const inputElements = document.getElementsByTagName("input");
-    usernameFields = Array.from(inputElements).filter(
-      (b) => isInferredUsernameField(b) === true
-    );
-  } catch (error) {
-    // console.log(`Error occured while finding username elements: `, error.message);
-  }
-  return usernameFields.map((element) => ({
-    fieldName: "username",
-    element,
-    isShown: isShown(element),
-  }));
+  const inputElements = document.getElementsByTagName("input");
+  return Array.from(inputElements)
+    .filter(isInferredUsernameField)
+    .map((element) => ({
+      fieldName: "username",
+      element,
+      isShown: isShown(element),
+    }));
 }
 
 function getPasswordFields() {
-  let passwordFields = [];
-  try {
-    passwordFields = [...document.querySelectorAll("input[type=password]")];
-  } catch (error) {
-    // console.log(`Error occured while finding password elements: `, error.message);
-  }
-  return passwordFields.map((element) => ({
+  return [...document.querySelectorAll("input[type=password]")].map((element) => ({
     fieldName: "password",
     element,
     isShown: isShown(element),
@@ -101,41 +79,35 @@ function getPasswordFields() {
 }
 
 function getEmailsByFathom() {
-  let emailsByFathom = [];
   try {
-    emailsByFathom = [...detectEmailInputs(document)];
+    return [...detectEmailInputs(document)].map((element) => ({
+      fieldName: "email",
+      element,
+      isShown: isShown(element),
+    }));
   } catch (error) {
-    // console.log(`Error occured while finding password elements: `, error.message);
+    console.log(`Error occured while finding email elements: ${error.message}`);
+    return [];
   }
-  return emailsByFathom.map((element) => ({
-    fieldName: "email",
-    element,
-    isShown: isShown(element),
-  }));
 }
 
 function getAllPIIFields(isShown) {
-  let allPIIFields = [];
   const autofillFields = getAutofillElements();
-  allPIIFields = allPIIFields.concat(autofillFields);
   const usernameFields = getUsernameFields();
-  let onlyInUsernameFields = usernameFields.filter(function (obj) {
-    return autofillFields.map((el) => el.element).indexOf(obj.element) == -1;
-  });
-  if (onlyInUsernameFields.length) {
-    allPIIFields = allPIIFields.concat(onlyInUsernameFields);
-  }
   const emailFieldsByFathom = getEmailsByFathom();
-  let onlyInEmailsByFathom = emailFieldsByFathom.filter(function (obj) {
-    return allPIIFields.map((el) => el.element).indexOf(obj.element) == -1;
-  });
-  if (onlyInEmailsByFathom.length) {
-    allPIIFields = allPIIFields.concat(onlyInEmailsByFathom);
-  }
-  let passwordFields = getPasswordFields();
-  allPIIFields = allPIIFields.concat(passwordFields);
-  if (isShown) {
-    allPIIFields = allPIIFields.filter((element) => element.isShown);
-  }
-  return allPIIFields;
+  const passwordFields = getPasswordFields();
+
+  const allPIIFields = [
+    ...autofillFields,
+    ...usernameFields.filter(
+      (obj) => autofillFields.map((el) => el.element).indexOf(obj.element) === -1
+    ),
+    ...emailFieldsByFathom.filter(
+      (obj) =>
+        allPIIFields.map((el) => el.element).indexOf(obj.element) === -1
+    ),
+    ...passwordFields,
+  ];
+
+  return isShown ? allPIIFields.filter((element) => element.isShown) : allPIIFields;
 }
