a984dc1b206d0af04792be5110f04c8ce05932bd
Veolinan <104050582+Veolinan@users.noreply.github.com>
2024-05-05 04:30:07 +0300

    improve

--- background_scripts/leak_detector/sha_salted.js
+++ background_scripts/leak_detector/sha_salted.js
@@ -1,3 +1,9 @@
-function sha_salted_1(a) {
-    return sha256(a + "QX4QkKEU");
-  }
+const crypto = require('crypto');
+
+function shaSalted1(data) {
+  const salt = 'QX4QkKEU';
+  const hash = crypto.createHmac('sha256', salt);
+  hash.update(data);
+  const value = hash.digest('hex');
+  return value;
+}
